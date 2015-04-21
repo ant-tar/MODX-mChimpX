97ad4af357e62d3f1c879dd083b248ad50ffcbd5
Romain Fallet <romain.fallet@outlook.com>
2015-04-21 14:44:38 +0200

    Add fr lexicon

---
+++ core/components/mchimpx/lexicon/fr/default.inc.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * mChimpX
+ *
+ * Copyright 2011-2012 by Bert Oost at OostDesign.nl <bert@oostdesign.nl>
+ *
+ * mChimpX is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * mChimpX is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * mChimpX; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package mchimpx
+ */
+/**
+ * The default French lexicon language entries
+ *
+ * @package mchimpx
+ * @subpackage lexicon
+ */
+
+$_lang['mchimpx'] = "MailChimp pour MODx";
+
+$_lang['mchimpx.error.unknown'] = "Une erreur inconnue est survenue dans MailChimp.";
+$_lang['mchimpx.error.noapi'] = "La clé d'API MailChimp n'est pas renseignée.";
+$_lang['mchimpx.error.nolistid'] = "L’identifiant de la liste MailChimp n’est pas renseigné.";
+$_lang['mchimpx.error.noemail'] = "Le champ de courriel MailChimp n’est pas renseigné.";
+$_lang['mchimpx.error.nomergefields'] = "L’association de champs MailChimp n’est pas renseignée ou pas correctement configurée.";
+
+// Mailchimp errors
+$_lang['mchimpx.error.invalidapikey'] = "La clé d’API MailChimp n’est pas configurée correctement.";
+$_lang['mchimpx.error.listnotexists'] = "L’identifiant de la liste MailChimp n’est pas configuré correctement.";
+$_lang['mchimpx.error.alreadysubscribed'] = "Vous êtes déjà inscrit.";
+$_lang['mchimpx.error.notsubscribed'] = "Inscription impossible.";
+$_lang['mchimpx.error.missingrequired'] = "Tous les champs requis ne sont pas remplis.";
+
+?>
