5906c7e141b963c2f55febcfe8cb3ae58074bc97
Anton Tarasov <himurovich@gmail.com>
2019-04-10 18:58:16 +0300

    proper BASE_PATH value

--- _build/build.config.php
+++ _build/build.config.php
@@ -6,7 +6,15 @@
  * In some cases, you may have to hard-code the full paths
  */
 
-define('MODX_BASE_PATH', dirname(dirname(dirname(dirname(__FILE__)))) . '/development/');
+if (isset($_SERVER['MODX_BASE_PATH'])) {
+	define('MODX_BASE_PATH', $_SERVER['MODX_BASE_PATH']);
+}
+elseif (file_exists(dirname(dirname(dirname(__FILE__))) . '/core')) {
+	define('MODX_BASE_PATH', dirname(dirname(dirname(__FILE__))) . '/');
+}
+else {
+	define('MODX_BASE_PATH', dirname(dirname(dirname(dirname(__FILE__)))) . '/');
+}
 define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
 define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
 define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
