e1667efedcc10d107ad698e0e51b25fe2ca3d1b4
Anton Tarasov <himurovich@gmail.com>
2019-04-16 11:38:06 +0300

    New RU translition

---
+++ core/components/mchimpx/lexicon/ru/default.inc.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * mChimpX
+ *
+ * Copyright 2011-2012 by Bert Oost at OostDesign.nl <bert@oostdesign.nl>
+ * Support and maintenance from 2019 by Anton Tarasov <contact@antontarasov.com>
+ *
+ * mChimpX is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * mChimpX is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * mChimpX; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package mchimpx
+ */
+/**
+ * The default Russian lexicon language entries
+ *
+ * @package mchimpx
+ * @subpackage lexicon
+ */
+
+$_lang['mchimpx'] = "Mailchimp для MODX";
+
+$_lang['mchimpx.error.unknown'] = "Возникла неизвестная ошибка Mailchimp!";
+$_lang['mchimpx.error.noapi'] = "Не указан ключ Mailchimp API!";
+$_lang['mchimpx.error.nolistid'] = "Не указан ID Mailchimp 'аудитории'(списка)!";
+$_lang['mchimpx.error.noemail'] = "Не указано поле email для Mailchimp!";
+$_lang['mchimpx.error.nomergefields'] = "Отсутствуют (или неверно сконфигурированы) поля для слияния и передачи в Mailchimp!";
+
+// Mailchimp errors
+$_lang['mchimpx.error.invalidapikey'] = "Ключ Mailchimp API сконфигурирован неверно!";
+$_lang['mchimpx.error.listnotexists'] = "ID аудитории/ списка Mailchimp в listId не сконфигурирован!";
+$_lang['mchimpx.error.alreadysubscribed'] = "Вы уже были подписаны самостоятельно!";
+$_lang['mchimpx.error.notsubscribed'] = "Подписка невозможно!";
+$_lang['mchimpx.error.missingrequired'] = "Не все обязательные поля заполнены!";
